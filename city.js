28c25d1c967659aafb7cac5f67f66eb86f1e157f
Divyashree Y <136325917+Divyashreey@users.noreply.github.com>
2023-07-18 14:02:47 +0530

    IBM project uploaded

---
+++ city.js
@@ -0,0 +1,39 @@
+export const city=[
+    {
+        name:"RS Puram",
+        place:289,
+    },
+    {
+        name:"Peelamedu",
+        place:425,
+    },
+    {
+        name:"Race Course",
+        place:84,
+    },
+    {
+        name:"Saibaba Colony",
+        place:209,
+    },
+    {
+        name:"Gandipuram",
+        place:240,
+    },
+    {
+        name:"Townhall",
+        place:220,
+    },
+    {
+        name:"Ramanathapuram",
+        place:146,
+    },
+    {
+        name:"Kalapatti",
+        place:68,
+    },
+    {
+        name:"see more",
+        place:"",
+        class:"seemore"
+     },
+]
